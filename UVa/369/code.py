c117f108f6f1f83a9725222af02ef34ea87851fd
Fernando Spaniol <fspaniol@gmail.com>
2018-07-30 21:08:38 -0300

    Update UVA 369

--- UVa/369/code.py
+++ UVa/369/code.py
@@ -1,19 +1,14 @@
 #!/bin/python3
 
 import sys
+values = [0] * 102
+values[0] = 1
+for i in range(1, 102):
+    values[i] = values[i-1] * i
 
-def fat(n):
-    f = 1
-    for i in range(2,n+1):
-        f = f * i
-    return f
-
-def calc(n, m):
-    return fat(n) / (fat(n-m) * fat(m))
-
-if __name__ == '__main__':
-    while True:
-        i = list(map(lambda x: int(x), sys.stdin.readline().split()))
-        if(len(i) != 2 or (i[0] == 0 and i[1] == 0)):
-            break
-        print("{} things taken {} at a time is {} exactly.".format(i[0],i[1], int(calc(i[0], i[1]) )) )
+while True:
+    i = list(map(lambda x: int(x), sys.stdin.readline().split()))
+    if(len(i) != 2 or (i[0] == 0 and i[1] == 0)):
+        break
+    print("{} things taken {} at a time is {} exactly.".format(
+        i[0], i[1], int(values[i[0]] / (values[i[0] - i[1]] * values[i[1]]))))
